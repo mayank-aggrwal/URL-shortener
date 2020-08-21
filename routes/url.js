b4eba9f52a7779c94efb06341b6c0bbf3046e7bf
Mayank <aggarwal.mayank49@gmail.com>
2020-08-22 04:10:53 +0530

    Add route for custom code generation

--- routes/url.js
+++ routes/url.js
@@ -52,5 +52,53 @@ router.post('/short', async (req, res) => {
 });
 
 
+// @route        POST /api/custom
+// @desc         Create custom short URL
+router.post('/custom', async (req, res) => {
+    const longUrl = req.body.longUrl;
+    const customCode = req.body.code;
+    const baseUrl = config.get('baseUrl');
+
+    if(!validUrl.isUri(baseUrl)) {
+        return res.status(401).json(`Invalid base URL`);
+    }
+
+    // Check long URL
+    if(validUrl.isUri(longUrl)) {
+        try {
+            let url = await Url.findOne({ longUrl });
+
+            if(url) {
+                res.json(url);
+            }
+            else {
+                let code = await Url.findOne({ urlCode: customCode });
+                if(code) {
+                    res.status(401).json(`Code already in use`);
+                }
+                else {
+                    const shortUrl = baseUrl + '/' + customCode;
+                    url = new Url({
+                        longUrl,
+                        shortUrl,
+                        urlCode: customCode,
+                        date: new Date()
+                    });
+                    await url.save();
+                    res.json(url);
+                }
+            }
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).json(`Server error`);
+        }
+    }
+    else {
+        console.log(`Invalid long URL`);
+        res.status(401).json('Invalid long URL');
+    }
+});
+
 
 module.exports = router;
